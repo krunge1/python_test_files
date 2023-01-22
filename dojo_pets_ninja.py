bb4ea0aff8c5e171720cfd1a4037358e25b098f7
Kyle Runge <krunge1@hotmail.com>
2023-01-21 20:26:35 -0600

    dojo_pets

---
+++ dojo_pets_ninja.py
@@ -0,0 +1,11 @@
+import dojo_pets_pets
+
+class Ninja:
+    def __init__(self, first_name, last_name, pet, treats, pet_food):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.pet = pet
+        self.treats = treats
+        self.pet_food = pet_food
+
+
---
+++ dojo_pets_pets.py
@@ -0,0 +1,5 @@
+import dojo_pets_ninja
+
+class Pets:
+    def __init__(self) -> None:
+        pass
